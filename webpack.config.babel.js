77682c8131f0d99eab20413719f0b906373244fe
Ryan Hsu <mvpdw06@gmail.com>
2017-02-23 21:45:50 +0800

    change webpack.config.babel.js settings. It can change development / production mode.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,30 +1,63 @@
 import path from 'path';
 import webpack from 'webpack';
 
-export default {
-	entry: [
-		'react-hot-loader/patch',
+const env = process.env.NODE_ENV || 'development';
+const isDevEnv = env === 'development';
+
+// webpack settings
+const getDevtoolSetting = () => {
+    return isDevEnv ?
+    'cheap-module-eval-source-map' :
+    'cheap-module-source-map';
+}
+const getPluginsSetting = () => {
+    const plugins = [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(env)
+        })
+    ];
+    if (isDevEnv) {
+        plugins.push(
+            new webpack.HotModuleReplacementPlugin()
+        );
+    } else {
+        plugins.push(
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            })
+        );
+    }
+    return plugins;
+}
+
+const webpackConfig = {
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: [/node_modules/]
+            }
+        ]
+    },
+    devtool: getDevtoolSetting(),
+    plugins: getPluginsSetting()
+}
+
+const AppConfig = Object.assign({}, webpackConfig, {
+    entry: [
+        'react-hot-loader/patch',
 		path.join(__dirname, 'src/index'),
-		'webpack-dev-server/client?http://localhost:3000',       
+		'webpack-dev-server/client?http://localhost:3000',
 		'webpack/hot/only-dev-server'
-	],
-	output: {
-		path: path.join(__dirname, 'output/assets'),
+    ],
+    output: {
+        path: path.join(__dirname, 'output/assets'),
 		filename: 'app.js',
 		publicPath: '/assets'
-	},
-	// resolve: "",
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-	],
-	module: {
-		loaders: [
-			{
-				test: /\.js$/, 
-				loader: 'babel', 
-				exclude: [/node_modules/]
-			}
-		]
-	},
-	// devtool: ""
-}
+    }
+});
+
+module.exports = AppConfig;
