c1b21181a041ff08d820dd8f3816fffeb72e20b2
Ryan Hsu <mvpdw06@gmail.com>
2017-02-26 17:20:16 +0800

    add thunkMiddleware logic and implement applyMiddleware.

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,22 @@
 import reducer from '../reducer/reducer';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { devToolsEnhancer } from 'redux-devtools-extension';
 
 const reduxDevTool = devToolsEnhancer(
     // Specify here name, actionsBlacklist, actionsCreators and other options if needed
 );
 
-const store = createStore(reducer, reduxDevTool);
+const thunkMiddleware = ({ dispatch, getState }) => {
+  return (next) => (action) => {
+    if (typeof action === 'function') {
+      return action(dispatch, getState);
+    }
+    return next(action);
+  };
+};
+
+const middleware = applyMiddleware(thunkMiddleware);
+
+const store = createStore(reducer, middleware, reduxDevTool);
 
 module.exports = store;
