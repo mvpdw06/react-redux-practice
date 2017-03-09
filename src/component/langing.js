48645f2991a1ab3a318db01a5ffcf61d28a14006
Ryan Hsu <mvpdw06@gmail.com>
2017-03-09 13:38:49 +0800

    Add landing page component for indexRoute.

---
+++ src/component/langing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Landing = (props) => (
+  <div>
+    <h1>React-redux practice</h1>
+    <h2>this is landing page.</h2>
+  </div>
+);
+
+module.exports = Landing;
--- src/component/routerComponent.js
+++ src/component/routerComponent.js
@@ -1,4 +1,5 @@
 import App from './app';
+import Landing from './langing';
 import Timer from './timer/timer';
 import Counter from './counter/counter';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
@@ -7,6 +8,7 @@ import React from 'react';
 const RouterComponent = (props) => (
   <Router history={hashHistory}>
     <Route path="/" component={App}>
+      <IndexRoute component={Landing} />
       <Route path="/counter" component={Counter} />
       <Route path="/timer" component={Timer} />
     </Route>
